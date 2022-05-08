1e25eef7a323e5466efcdcd0e8a5abd5814b10e2
Rafael Ortz <rd.ortizr1@uniandes.edu.co>
2022-05-08 14:22:50 -0500

    login report

--- features/login.feature
+++ features/login.feature
@@ -3,17 +3,31 @@ Feature: Login
 @user1 @web
 Scenario: Login Successfully
   Given I navigate to page "http://localhost:2368/ghost/#/signin"
-  When  I enter email 'rd.ortizr1@uniandes.edu.co'
-  When  I enter password '@CruelSun1802'
-  And   I click "login"
-  And   I wait for 5 seconds
+  When  I enter email "<USERNAME>"
+  When  I enter password "<PASSWORD>"
+  And   I click on element having class "login"
+  And   I wait for 7 seconds
   Then  I see admin site
 
 @user2 @web
 Scenario: Login Failed
   Given I navigate to page "http://localhost:2368/ghost/#/signin"
-  When  I enter email 'wrong_email'
-  When  I enter password 'wrong_password'
-  And   I click "login"
-  And   I wait for 5 seconds
+  When  I enter email "<PASSWORD>"
+  When  I enter password "<USERNAME>"
+  And   I click on element having class "login"
+  And   I wait for 7 seconds
   Then  I see login error
+
+@user3 @web
+Scenario: Login Failed
+  Given I navigate to page "http://localhost:2368/ghost/#/signin"
+  When  I enter email "<USERNAME>"
+  When  I enter password "<PASSWORD>"
+  And   I click on element having class "login"
+  And   I wait for 7 seconds
+  When  I see admin site
+  And   I wait for 2 seconds
+  And   I click on element having class "gh-user-avatar"
+  And   I click on element having class "user-menu-signout"
+  And   I wait for 1 seconds
+  Then  I validate exists class element "gh-signin"
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,31 +1,60 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const { faker } = require('@faker-js/faker');
 
-When('I enter email {string}', async function (email) {
+//Login
+When('I enter email {kraken-string}', async function (email) {
     let element = await this.driver.$('.email');
-
     return await element.setValue(email);
 });
 
-When('I enter password {string}', async function (password) {
+When('I enter password {kraken-string}', async function (password) {
     let element = await this.driver.$('.password');
-
     return await element.setValue(password);
-});
-
-Then('I click {string}', async function (string) {
-    let element = await this.driver.$('.' + string);
-
-    return await element.click();
 });
 
 Then('I see admin site', async function () {
     let element = await this.driver.$('.ember-application');
-
    return await element
 });
 
 Then('I see login error', async function () {
     let element = await this.driver.$('.main-error');
-
    return await element
+});
+
+Then('I click on element having class {string}', async function (string) {
+    let element = await this.driver.$('.' + string);
+    return await element.click();
+});
+
+Then('I click on element having id {string}', async function (string) {
+    let element = await this.driver.$('#' + string);
+    return await element.click();
+});
+
+//Posts
+Then('I enter {string} into value {string}', async function (string1, string2) {
+    let element = await this.driver.$('#' + string1);
+    return await element.setValue(string2);
+});
+
+Then('I click on element having attribute {string}', async function (string) {
+    let element = await this.driver.$(string);
+    return await element.click();
+});
+
+Then('I enter random string into attribute {string}', async function (id) {
+    let element = await this.driver.$(id);
+    return await element.setValue(faker.datatype.string());
+});
+
+
+Then('I validate draft post list item', async function () {
+    let element = await this.driver.$('.gh-posts-list-item');
+    return await element;
+});
+
+Then('I validate exists class element {string}', async function (string) {
+    let element = await this.driver.$('.' + string);
+    return await element;
 });
